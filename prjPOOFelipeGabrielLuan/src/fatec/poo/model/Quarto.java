093ee15d6a1918376dd13c77ab2601b13568e9d0
Gabriel Caetano <110908037+c4etan0w@users.noreply.github.com>
2023-10-23 08:09:53 -0300

    Add files via upload

---
+++ prjPOOFelipeGabrielLuan/src/fatec/poo/model/Quarto.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fatec.poo.model;
+
+/**
+ *
+ * @author 0030482211022
+ */
+public class Quarto {
+    private int numero;
+    private String tipo;
+    private boolean situacao;
+    private double valorDiaria;
+    private double totalFaturado;
+    private Registro registro;
+
+    public Quarto(int numero, String tipo, double valorDiaria) {
+        this.numero = numero;
+        this.tipo = tipo;
+        this.valorDiaria = valorDiaria;
+        this.registro = null;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public boolean isSituacao() {
+        return situacao;
+    }
+
+    public double getValorDiaria() {
+        return valorDiaria;
+    }
+
+    public double getTotalFaturado() {
+        return totalFaturado;
+    } public Registro getRegistro() {
+        return registro;
+    }
+
+    public void setRegistro(Registro registro) {
+        this.registro = registro;
+    }
+
+
+    public void reservar() {
+        this.situacao = true;
+    }
+
+    public double liberar(int qtdDiasHospedado) {
+        this.situacao = false;
+        double valorHospedagem = this.valorDiaria * qtdDiasHospedado;
+        this.totalFaturado += valorHospedagem;
+
+        return valorHospedagem;
+    }
+}
---
+++ prjPOOFelipeGabrielLuan/src/fatec/poo/model/Registro.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fatec.poo.model;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author 0030482211022
+ */
+public class Registro {
+    private int codigo;
+    private LocalDate dataEntrada;
+    private LocalDate dataSaida;
+    private double valorHospedagem;
+    private Recepcionista recepcionista;
+    private Quarto quarto;
+    private Hospede hospede;
+    private ArrayList<ServicoQuarto> servicos;
+
+    public Registro(int codigo, LocalDate dataEntrada,Recepcionista recepcionista  ) {
+        this.codigo = codigo;
+        this.dataEntrada = dataEntrada;
+        this.recepcionista = recepcionista;
+    }
+
+    public void setDataSaida(LocalDate dataSaida) {
+        this.dataSaida = dataSaida;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public LocalDate getDataEntrada() {
+        return dataEntrada;
+    }
+
+    public LocalDate getDataSaida() {
+        return dataSaida;
+    }
+
+    public double getValorHospedagem() {
+        return valorHospedagem;
+    }
+
+    public Recepcionista getRecepcionista() {
+        return recepcionista;
+    }
+
+    public void setRecepcionista(Recepcionista recepcionista) {
+        this.recepcionista = recepcionista;
+    }
+
+    public Hospede getHospede() {
+        return hospede;
+    }
+
+    public void setHospede(Hospede hospede) {
+        this.hospede = hospede;
+    }
+    public Quarto getQuarto() {
+        return quarto;
+    }
+
+    public void setQuarto(Quarto quarto) {
+        this.quarto = quarto;
+    }
+
+
+    public ArrayList<ServicoQuarto> getServicos() {
+        return servicos;
+    }
+
+    public void adicionarServico(ServicoQuarto servico) {
+        this.servicos.add(servico);
+    }
+
+    public void reservarQuarto(Hospede hospede, Quarto quarto) {
+	this.hospede = hospede;
+        this.quarto = quarto;
+        quarto.reservar();
+    }
+
+	public double liberarQuarto() {
+   double valorHospedagem = this.quarto.getValorDiaria() * (this.dataSaida.getDayOfYear() - this.dataEntrada.getDayOfYear());
+
+
+    if (this.hospede.getTaxaDesconto() > 0) {
+        valorHospedagem = valorHospedagem * (1 - this.hospede.getTaxaDesconto());
+    }
+
+    double valorServicos = 0;
+    for (ServicoQuarto servicoQuarto : this.servicos) {
+        valorServicos += servicoQuarto.getValor();
+    }
+
+    return valorHospedagem + valorServicos;
+}
+}
